2944fffc898dd3cd45f243074c7bb527574680a0
Raul <raulvaliturbide@gmail.com>
2025-02-27 17:25:59 +0100

    Update Jugador_Clase.java

--- src/Gestion/Socio.java
+++ src/Gestion/Socio.java
@@ -3,52 +3,60 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Gestion;
+
 import java.util.Comparator;
 import java.util.Collections;
+
 /**
  *
  * @author baske
  */
 public class Socio implements Comparable<Socio> {
-        
 
     private String nombre;
     private String DNI;
     private double aportacion;
     private int edad;
-    
-    public Socio(String nombre,String DNI, double aportacion,int edad){
+
+    public Socio(String nombre, String DNI, double aportacion, int edad) {
         this.nombre = nombre;
         this.DNI = DNI;
         this.aportacion = aportacion;
         this.edad = edad;
     }
-    
-    public String getNombre(){
+
+    public String getNombre() {
         return nombre;
     }
-    public void setNombre(String nombre){
+
+    public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    public String getDNI(){
+
+    public String getDNI() {
         return DNI;
     }
-    public void setDNI(String DNI){
+
+    public void setDNI(String DNI) {
         this.DNI = DNI;
     }
-    public int getEdad(){
+
+    public int getEdad() {
         return edad;
     }
-    public void setEdad(int edad){
+
+    public void setEdad(int edad) {
         this.edad = edad;
     }
-    public double getAportacion(){
+
+    public double getAportacion() {
         return aportacion;
     }
-    public void setAportacion(double aportacion){
+
+    public void setAportacion(double aportacion) {
         this.aportacion = aportacion;
     }
-    
+
     @Override
     public String toString() {
         return String.format("""
@@ -56,12 +64,15 @@ public class Socio implements Comparable<Socio> {
                           Nombre: %s
                           Aportacion: %.2f
                           Edad: %d
-                          """, DNI, nombre, aportacion,edad);
+                          """, DNI, nombre, aportacion, edad);
 
     }
+
     /**
-     * -1 es cuando el objeto que se está comparando es mayor que el objeto en los argumentos
-     * 1 es cuando el objeto que se está comparando es menor que el objeto en los argumentos
+     * -1 es cuando el objeto que se está comparando es mayor que el objeto en
+     * los argumentos 1 es cuando el objeto que se está comparando es menor que
+     * el objeto en los argumentos
+     *
      * @param o el objeto a comparar
      * @return -1,0,1
      */
@@ -76,29 +87,19 @@ public class Socio implements Comparable<Socio> {
         }
     }
 
-   public static final Comparator<Socio> compararDNI = new Comparator<Socio>() { 
-    @Override
-    public int compare(Socio s1, Socio s2) {
-        return s1.getDNI().toLowerCase().compareTo(s2.getDNI().toLowerCase());
+    public static void sumaDNI(Socio s) {
+        int resultado = 0;
+        String Dni = s.DNI.substring(0, 7);
+
     }
-   };
-   
-   public static void sumaDNI(Socio s){
-       String Dni = s.DNI.substring(0,8);
-       System.out.println(Dni);
-   }
-    
-    public static final Comparator<Socio> compararEdad = new Comparator<Socio>(){
-        
+
+    public static final Comparator<Socio> compararEdad = new Comparator<Socio>() {
+
         @Override
-        public int compare(Socio s1, Socio s2){
-        return Integer.compare(s2.getEdad(),s1.getEdad());
-                }
-    
+        public int compare(Socio s1, Socio s2) {
+            return Integer.compare(s2.getEdad(), s1.getEdad());
+        }
+
     };
+
 }
-
-
-    
-    
-
--- src/Grafica/Menu.java
+++ src/Grafica/Menu.java
@@ -3,19 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Grafica;
+
 import java.util.Scanner;
 import java.util.InputMismatchException;
+
 /**
  *
  * @author baske
  */
 public class Menu {
+
     public static Scanner teclado = new Scanner(System.in);
-    
-    public static void limpiarBuffer(){
+
+    public static void limpiarBuffer() {
         teclado.nextLine();
     }
-    
+
     public static int introduccion() {
         int usuario = 0;
         boolean correcto = false;
@@ -37,11 +40,15 @@ public class Menu {
                 System.out.println("Erro desconocido, intentelo de nuevo");
             }
         }
-    return usuario;}
-    
+        return usuario;
+    }
+
     /**
-     * El objetivo de este metodo es que el usuario decida por cual atributo desea listar
-     * @return  un numero que activará el switch desde la main para ejecutar un listado u otro
+     * El objetivo de este metodo es que el usuario decida por cual atributo
+     * desea listar
+     *
+     * @return un numero que activará el switch desde la main para ejecutar un
+     * listado u otro
      */
     public static int queListar() {
         int usuario = 0;
@@ -51,28 +58,31 @@ public class Menu {
                            1-Aportacion(Mayor a Menor)
                            2-Edad (Mayor a Menor)
                            """);
-        while (!margenDatos(1,2,usuario)) {
+        while (!margenDatos(1, 2, usuario)) {
             try {
                 System.out.print(">>");
                 usuario = teclado.nextInt();
                 correcto = true;
             } catch (InputMismatchException e) {
                 System.out.println("Introduzca un numero e intentelo de nuevo");
-            }catch(Exception e){
-                System.out.println("Error desconocido, intentelo de nuevo");}
+            } catch (Exception e) {
+                System.out.println("Error desconocido, intentelo de nuevo");
+            }
         }
 
         return usuario;
     }
+
     /**
      * El objetivo de este metodo es marcar los min y max permitidos
+     *
      * @param min el numero (inclusive) minimo que marca el usuario
      * @param max el numero (inclusive) maximo que marca el usuario
      * @param usuario el numero que ha marcado el usuario
      * @return será false si no cumple los requisitos y true si cumple
      */
     private static boolean margenDatos(int min, int max, int usuario) {
-        
+
         if (usuario < min || usuario > max) {
             System.out.println("Por favor introduzca datos entre " + min + " y " + max);
             return false;
@@ -81,5 +91,4 @@ public class Menu {
         }
     }
 
-    
 }
